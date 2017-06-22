232559e822f7370ebba878cf46f38c78cdd30fd5
Sean Hughes <smhughes@uw.edu>
2017-06-21 21:07:39 -0700

    do a better job of adding time and game information to the history of the model rating

--- model.R
+++ model.R
@@ -27,17 +27,26 @@ history <- s$history %>%
     as.data.frame() %>% 
     gather(Team, Rating) %>% 
     mutate(Time = rep(unique(game_data$Date), nrow(s$history)), 
-        Time = lubridate::ymd_hms(Time))
+        Time = lubridate::ymd_hms(Time), 
+        Game = rep(1:ncol(s$history),  nrow(s$history))) %>% 
+    as_data_frame()
 
-seasons <- game_data %>% 
-    group_by(Season, Year) %>% 
-    summarise(Time = lubridate::ymd_hms(min(Date)))
+games <- game_data %>% 
+    group_by(Season, Week, Date) %>% 
+    summarise(Year = Year[1]) 
+
+history <- merge(games, history, by.y = "Time", by.x = "Date") %>% 
+    as_data_frame()
 
 history %>% 
     filter(Team %in% c("Stranger Danger", "Sluggernauts",
         "Balls Deep", "Hitmen", "Flesh Eating Sharks")) %>%
-    ggplot(aes(x = Time, y = Rating, color = Team)) + 
+    filter(Year == 2017) %>% 
+    ggplot(aes(x = Game, y = Rating, color = Team)) + 
         geom_step() +
-#        geom_vline(xintercept = as.numeric(seasons$Time), color = "black") + 
         scale_color_brewer(type = "qual", palette = 2)
+
+expected_score <- function(team_ranking, opponent_ranking) {
+    10^(team_ranking/400) / (10^(team_ranking/400) + 10^(opponent_ranking/400))
+}
 
