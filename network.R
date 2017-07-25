0e031d6a94dfee9096a26e0182ec8db6f8922534
Sean Hughes <smhughes@uw.edu>
2017-07-24 20:10:12 -0700

    Remove redundant network plot and scale nodes appropriately

--- network.R
+++ network.R
@@ -35,14 +35,8 @@ ggraph(attempt, layout = 'kk') +
     geom_edge_fan(aes(color = Winner), show.legend = TRUE) + 
     geom_node_point(aes(color = Team, size = Winningness)) + 
     scale_edge_color_brewer(type = "qual", palette = "Paired") + 
-    scale_color_brewer(type = "qual", palette = "Paired", guide = FALSE)
-
-ggraph(attempt, layout = 'kk') + 
-    geom_edge_fan(aes(color = Winner), show.legend = TRUE) + 
-    geom_node_point(aes(color = Team, size = Winningness)) + 
-    scale_edge_color_brewer(type = "qual", palette = "Paired") + 
-    scale_color_brewer(type = "qual", palette = "Paired", guide = FALSE)
-
+    scale_color_brewer(type = "qual", palette = "Paired", guide = FALSE) + 
+    scale_size(limits = c(0, 100))
 
 # brewer colors work (bc sequential) but ggplot auto colors are fucked
 # because diff colors assigned depending on # of levels bc of tie level 
