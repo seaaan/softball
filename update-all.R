a1042d82db1b9c8b826454db1aba487e4e9fb868
Sean Hughes <smhughes@uw.edu>
2017-08-26 13:12:07 -0700

    Run the analyses automatically from update-all.R

--- update-all.R
+++ update-all.R
@@ -7,4 +7,8 @@ source("R/scrape-data.R")
 # Update the models based on the latest game outcomes.
 source("R/model.R")
 
-# 
+# Run the scouting report analysis on the team for next week
+rmarkdown::render("Scouting-report.Rmd")
+
+# Run the scoring distribution analysis
+rmarkdown::render("How-to-win-a-game.Rmd")
